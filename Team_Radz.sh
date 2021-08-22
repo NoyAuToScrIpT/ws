959875bc74dd2a54009bcb537b1f4b8df216916a
NoyAuToScrIpT <88473101+NoyAuToScrIpT@users.noreply.github.com>
2021-08-22 10:56:26 +0800

    Add files via upload

---
+++ Team_Radz.sh
@@ -0,0 +1,8 @@
+
+##Download Script First
+wget https://raw.githubusercontent.com/NoyAuToScrIpT/ws/main/team_radz.py
+##Set to run at startup
+sudo cp -i /root/team_radz.py /bin
+##Cron Job
+sudo crontab -e
+##Stop na dito haha

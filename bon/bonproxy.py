e0ef4df2db73ad80fea46117e34e06c34290dbef
NoyAuToScrIpT <88473101+NoyAuToScrIpT@users.noreply.github.com>
2021-09-07 10:01:20 +0800

    Update bonproxy.py

--- bon/bonproxy.py
+++ bon/bonproxy.py
@@ -2,7 +2,7 @@ import socket, threading, thread, select, signal, sys, time, getopt
 
 # CONFIG
 LISTENING_ADDR = '0.0.0.0'
-LISTENING_PORT = 80
+LISTENING_PORT = 443
 
 PASS = ''
 
@@ -226,7 +226,7 @@ class ConnectionHandler(threading.Thread):
 def print_usage():
     print 'Usage: proxy.py -p <port>'
     print '       proxy.py -b <bindAddr> -p <port>'
-    print '       proxy.py -b 0.0.0.0 -p 80'
+    print '       proxy.py -b 0.0.0.0 -p 443'
 
 def parse_args(argv):
     global LISTENING_ADDR
